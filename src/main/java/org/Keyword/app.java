0b057232a1d8e182434b0cf567465b3d5ae8a33e
Tuna-Telatar <tunatelatar@gmail.com>
2025-12-09 12:18:30 +0300

    commit 1

---
+++ src/main/java/org/Keyword/app.java
@@ -0,0 +1,14 @@
+package org.Keyword;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class app {
+    public static void main(String[] args) {
+        SpringApplication.run(app.class, args);
+
+
+    }
+}
+
